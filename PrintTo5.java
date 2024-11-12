48876ce22c0ccc40bc5244394a723cc2544a79c7
Daniel Yacobi <danieljoshua.yacobi@post.runi.ac.il>
2024-11-12 18:46:17 +0200

    Bill3 fourth

---
+++ PrintTo5.java
@@ -0,0 +1,12 @@
+// Prints the numbers 0 to 5.
+// (Same program as shown in lecture 1-1).
+public class PrintTo5 {
+	public static void main(String[] args) {
+        int i = 0; // Declares an int variable, and sets it to 0
+        while (i < 6) {
+            System.out.println(i); // Prints the current value of i
+            i = i + 1; // Makes i bigger by 1
+        }
+        System.out.println("done"); 
+    }
+ }
